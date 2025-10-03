1d9b80ad8c9859dc301bb294a1b586e98a0b45ff
pinpinneh <ervinnacdebora@gmail.com>
2025-10-03 09:18:45 +0700

    Update T00.java

--- T00.java
+++ T00.java
@@ -3,7 +3,7 @@ public class T00 {
 import java.util.*;
 import java.lang.Math;
 
-public class JavaApplication {
+public class T00 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
