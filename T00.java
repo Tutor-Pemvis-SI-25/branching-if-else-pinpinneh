91464769c23b37a3fe8058999495862e4e5b403b
pinpinneh <ervinnacdebora@gmail.com>
2025-10-03 09:44:20 +0700

    pair 26

--- T00.java
+++ T00.java
@@ -5,11 +5,11 @@ public class T00 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int bil1, bil2, bilganjil, bilgenap, perkalian, terbesar, penjumlahan, jenis, total1, total2;
+        int bil1, bil2, bilganjil, bilgenap, perkalian, terbesar, penjumlahan, jenis, total1, total2, sama;
 
-        System.out.println("Masukkan bilangan pertama:");
+        System.out.println("Bilangan pertama:");
         bil1 = Integer.parseInt(input.nextLine());
-        System.out.println("Masukkan bilangan kedua:");
+        System.out.println("Bilangan kedua:");
         bil2 = Integer.parseInt(input.nextLine());
         total1 = bil1 % 2;
         total2 = bil2 % 2;
@@ -25,10 +25,21 @@ public class T00 {
         }
         if (bil1 > bil2) {
             terbesar = bil1;
-            System.out.println("bilangan pertama lebih besar" + terbesar);
+            System.out.println("Bilangan kedua lebih besar" + terbesar);
         } else {
             terbesar = bil2;
-            System.out.println("bilangan kedua lebih besar" + terbesar);
+            System.out.println("Bilangan kedua lebih besar" + terbesar);
+        }
+        if (bil1 == bil2) {
+            sama = bil1;
+            System.out.println("Kedua bilangan sama besar");
+        } else {
+            sama = bil2;
+            System.out.println("Kedua bilangan sama besar");
+        }
+        if (total1 == 0 && total2 == 0) {
+            penjumlahan = bil1 + bil2;
+            System.out.println("hasil penjumlahan: " + penjumlahan);
         }
         if (total1 == 1 && total2 == 1) {
             perkalian = bil1 * bil2;
